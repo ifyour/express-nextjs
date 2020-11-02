614520803b0c0c76c509bcdc5bd5e6c1991bea78
ifyour <ifyour@outlook.com>
2020-11-02 21:25:00 +0800

    fix: statick files servering

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
-const dev = true;
 
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname, 'client/out')));
+// https://github.com/vercel/next.js/discussions/14532
+app.use(express.static(path.join(__dirname, '../client/public')))
+app.use('/_next', express.static(path.join(__dirname, '../client/out/_next')));
 
 // An api endpoint that returns a short list of items
 app.get('/api/getList', (req, res) => {
@@ -18,11 +18,6 @@ app.get('/api/getList', (req, res) => {
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/out/index.html'));
 });
-
-// Static files
-// https://github.com/vercel/next.js/discussions/14532
-app.use(express.static(path.join(__dirname, '../client/public')))
-app.use('/_next', express.static(path.join(__dirname, '../client/out/_next')));
 
 const port = process.env.PORT || 5000;
 app.listen(port);
