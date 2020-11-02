89cd95e0b32354384bd954a20a39742cf52df66a
ifyour <ifyour@outlook.com>
2020-11-02 21:15:01 +0800

    fix

--- client/pages/api/[...all].js
+++ client/pages/api/[...all].js
@@ -1,5 +1,6 @@
 import httpProxyMiddleware from 'next-http-proxy-middleware'
 
+// https://nextjs.org/docs/api-routes/dynamic-api-routes#optional-catch-all-api-routes
 export default (req, res) => (
   httpProxyMiddleware(req, res, {
     target: 'http://localhost:5000',
