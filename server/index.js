40f4e2a474bb2fc152bea47e7f9fb9c811346bb4
ifyour <ifyour@outlook.com>
2020-11-02 21:06:55 +0800

    fix: env config

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-
 const dev = true;
 
 // Serve the static files from the React app
@@ -33,4 +32,4 @@ app.use(
 const port = process.env.PORT || 5000;
 app.listen(port);
 
-console.log('App is listening on port ' + port);
+console.log('App is listening on port http://localhost:' + port);
